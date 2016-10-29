b93c3845d425408c0a74b2a6ab661ba688186aa7
Thomas1995 <thomas.suditu@gmail.com>
2016-10-30 00:01:05 +0300

    add method

--- Tema1/problema4.java
+++ Tema1/problema4.java
@@ -7,11 +7,13 @@ public class problema4 {
 
   private static class BinarySearchTree<K extends Comparable<K>, V> {
     private class Node<K extends Comparable<K>, V> {
+      K key;
       V value;
       Node<K,V> left, right;
 
-      public Node(V o) {
-        value = o;
+      public Node(K k, V v) {
+        key = k;
+        value = v;
         left = right = null;
       }
     }
@@ -23,7 +25,33 @@ public class problema4 {
     }
 
     public boolean add(K key, V val) {
-      
+      Node<K,V> node = root, ant = null;
+
+      while(node != null) {
+        if(node.key.compareTo(key) == 0)
+          return false;
+
+        if(node.key.compareTo(key) > 0) {
+          ant = node;
+          node = node.left;
+        }
+        else {
+          ant = node;
+          node = node.right;
+        }
+      }
+
+      if(ant == null) {
+        root = new Node<K,V>(key, val);
+      }
+      else {
+        if(ant.key.compareTo(key) > 0)
+          ant.left = new Node<K,V>(key, val);
+        else
+          ant.right = new Node<K,V>(key, val);
+      }
+
+      return true;
     }
 
     @Override
